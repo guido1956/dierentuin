56dc49b613ad3621cb32f8412737a5924f3ddd9b
Guido1956 <GuidoDulos@valori.nl>
2020-10-10 21:35:28 +0200

    US00 Dierentuin00 eerste opbouw EINDE

---
+++ src/main/java/Dierentuin00.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Dierentuin00 {
+    public static void main(String[] args) {
+        //String naamMijnDierentuin = "Valori's Zoo";
+        //String dier01 = "Zebravink";
+        //String soort01 = "Vogel";
+        //System.out.println("Mijn dierentuin heet " + naamMijnDierentuin);
+        //System.out.print("Mijn dier is een " + dier01);
+        //System.out.println(" van het soort " + soort01);
+        //String Dier01 = "Olifant";  // Dier01 en dier01 zijn twee verschillende variabelen!
+        //System.out.println("Ik heb twee dieren " + "een " + dier01 + " en een " + Dier01 );
+        //int leeftijd01 = 30;
+        //int leeftijd02 = 93;
+        //System.out.println("Samen zijn deze dieren " + (leeftijd01 + leeftijd02) + " jaar oud");
+        //System.out.println("En dus niet " +  leeftijd01 + leeftijd02 + " jaar oud");
+    }
+}
