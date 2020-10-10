c6bb37563ffc1b99d273b70f3a9ca0d5f01ea132
Guido1956 <GuidoDulos@valori.nl>
2020-10-10 21:47:50 +0200

    US00 Dierentuin00 eerste opbouw EINDE

---
+++ src/main/java/Dier.java
@@ -0,0 +1,45 @@
+public class Dier {
+    private String naam;
+    private String soort;
+    private int gewicht;
+    private int leeftijd;
+
+    public Dier(String naam, String soort) {
+        this.naam = naam;
+        this.soort = soort;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getSoort() {
+        return soort;
+    }
+
+    public void setSoort(String soort) {
+        this.soort = soort;
+    }
+
+    public int getGewicht() {
+        return gewicht;
+    }
+
+    public void setGewicht(int gewicht) {
+        this.gewicht = gewicht;
+    }
+
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+
+    public void setLeeftijd(int leeftijd) {
+        this.leeftijd = leeftijd;
+    }
+}
+
+
